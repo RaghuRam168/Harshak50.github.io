8b22ca8f133b93d10addfcdd787cbf7275809f04
Harshak50 <kondapuramharsha@gmail.com>
2020-09-24 12:55:54 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,48 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCJHS46Cw5oZvURLgWskzV9aF3WR19Smg0",
+    authDomain: "contactform-c0a0c.firebaseapp.com",
+    databaseURL: "https://contactform-c0a0c.firebaseio.com",
+    projectId: "contactform-c0a0c",
+    storageBucket: "contactform-c0a0c.appspot.com",
+    messagingSenderId: "846497232685",
+    appId: "1:846497232685:web:d9fbe7b0137a66b3cf05a4",
+    measurementId: "G-0F2RR4BBYD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var messagesRef = firebase.database().ref('messages');
+
+
+
+//LISTEN FOR FORM SUBMIT
+document.getElementById('contactform').addEventListener('submit',submitForm);
+
+//submit form
+function submitForm(e){
+  e.preventDefault();
+   var name =getInputVal('name');  
+   var name =getInputVal('email');  
+   var name =getInputVal('phone');  
+   var name =getInputVal('message');  
+  
+}
+
+saveMessage(name,email,phone,message)
+
+// function to get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+//save msg to firebase
+function saveMessage(name,email,phone,message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name:name,
+        email : email,
+        phone: phone,
+        message :message
+    });
+}
